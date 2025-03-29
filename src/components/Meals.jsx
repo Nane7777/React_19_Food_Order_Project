620b59aa86b2bb1bb2d64956c3ececb144e73a0a
Nane <gemine_nicolas@hotmail.com>
2025-03-29 17:42:29 +0100

    29-03-25 React 19-296

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,24 @@
-import {useEffect, useState} from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp";
+
+const requestConfig = {};
 
 export default function Meals() {
-    const [loadedMeals, setLoadedMeals] = useState([]);
+    const {
+        data: loadedMeals,
+        isLoading,
+        error,
+    } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
+    console.log(loadedMeals);
 
-            if (!response.ok) {
+    if (isLoading) {
+        return <p>Fetching meals...</p>
+    }
 
-            }
-
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-
-        fetchMeals();
-    }, []);
+    // if (!data) {
+    //     return <p>No meals found.</p>;
+    // }
 
     return (
         <ul id="meals">{loadedMeals.map((meal) => (
